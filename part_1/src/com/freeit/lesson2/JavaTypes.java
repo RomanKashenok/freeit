ac8b336c1c9617f93bcd2808a3147226b08c758c
RomanKashenok <kashenok.r@mail.ru>
2022-06-15 21:18:53 +0300

    lesson2: explanation of java types (with examples)

---
+++ part_1/src/com/freeit/lesson2/JavaTypes.java
@@ -0,0 +1,32 @@
+package com.freeit.lesson2;
+
+public class JavaTypes {
+
+    public static void main(String[] args) {
+        // Numeric
+        byte b = (byte) 128;     // -128 - 127
+        short s = 5;    // 5
+        int i = 5;      // 5
+        long l = 5;     // 5
+        float f = 0.5f;    // 0.7f
+        double d = 0.5;   // 0.7
+
+        // Characters
+        char c = 'c';
+        char c2 = 87;
+
+        // Logical
+        boolean tb = true;
+        boolean fb = false;
+
+        // Strings
+        String s1 = "Hello";
+        String s2 = "world";
+        String s3 = "Hello";
+        String s4 = s1 + s2;
+
+        System.out.println((int) 'c');
+        System.out.println(c2);
+    }
+
+}
